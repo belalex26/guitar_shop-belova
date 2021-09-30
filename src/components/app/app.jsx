9dff2853690d51ffd6ec3c84ce7547689fb63738
alex <alex.belova26@gmail.com>
2021-09-30 20:52:50 +0300

    подготовка проекта

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+// import PropTypes from 'prop-types';
+
+function App() {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  );
+}
+
+export default App;
